0cf5682c11db7366f389eb7cefe531e04a08cb06
RetrainIt <573528886@qq.com>
2021-03-09 10:51:14 +0800

    Update

--- neo_db/creat_graph.py
+++ neo_db/creat_graph.py
@@ -1,12 +1,12 @@
 from py2neo import Graph, Node, Relationship
 from config import graph
 
-with open("D:/Coding/Neo4jProjects/KGQA_HLM-master/raw_data/relation.txt", 'r', encoding='utf-8') as f:
+with open("../raw_data/relation.txt", 'r', encoding='utf-8') as f:
     for line in f.readlines():
         rela_array=line.strip("\n").split(",")
         print(rela_array)
-        graph.run("MERGE(p: Person{cate:'%s',Name: '%s'})"%(rela_array[3],rela_array[0]))
-        graph.run("MERGE(p: Person{cate:'%s',Name: '%s'})" % (rela_array[4], rela_array[1]))
+        graph.run("CREATE(p: Person{cate:'%s',Name: '%s'})"%(rela_array[3],rela_array[0]))
+        graph.run("CREATE(p: Person{cate:'%s',Name: '%s'})" % (rela_array[4], rela_array[1]))
         graph.run(
             "MATCH(e: Person), (cc: Person) \
             WHERE e.Name='%s' AND cc.Name='%s'\
